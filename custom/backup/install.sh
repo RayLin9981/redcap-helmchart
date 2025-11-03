ff29bcbc7f1a6e7b6a6ddec53e743743c344425d
Ray <ray@example.com>
2025-11-03 08:52:54 +0800

    update

--- custom/backup/install.sh
+++ custom/backup/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAMESPACE=${NAMESPACE:-redcap}  # 可透過環境變數指定，預設 redcap
-echo "📌 部署資源到 namespace: $NAMESPACE"
+echo "部署資源到 namespace: $NAMESPACE"
 
 kubectl apply -n ${NAMESPACE} -f  ./yaml
 kubectl apply -n ${NAMESPACE} -f  ./pvc
